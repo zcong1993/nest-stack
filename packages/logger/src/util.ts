c7fb7cea89855bcb7507a7e41e8ec9ab80606ba9
zcong1993 <zhangcong1992@gmail.com>
2020-11-19 11:49:06 +0800

    feat: v2

--- packages/logger/src/util.ts
+++ packages/logger/src/util.ts
@@ -1,40 +1,22 @@
 import { transports, format } from 'winston';
-import * as DailyRotateFile from 'winston-daily-rotate-file';
 import { INestApplication } from '@nestjs/common';
 import { WINSTON_MODULE_NEST_PROVIDER } from './winston.constants';
-import { nestLikeConsoleFormat } from './format';
 
 const isProdByEnv: () => boolean = () => process.env.NODE_ENV === 'prod';
 
 export const createDefaultLogConfig = (isProd: () => boolean = isProdByEnv) => {
-  if (isProd()) {
-    return {
-      transports: [
-        new DailyRotateFile({
-          createSymlink: true,
-          symlinkName: 'combine.log',
-          filename: 'combine-%DATE%.log',
-          datePattern: 'YYYY-MM-DD-HH',
-          dirname: './logs',
-          level: 'info',
-          maxSize: '100m',
-          maxFiles: '10d',
-        }),
-        new transports.File({
-          filename: 'error.log',
-          dirname: './logs',
-          level: 'error',
-        }),
-      ],
-    };
-  }
+  const prettyFormat = format.combine(
+    format.colorize(),
+    format.align(),
+    format.simple(),
+  );
+
+  const jsonFormat = format.json();
+  const outputFormat = isProd() ? jsonFormat : prettyFormat;
 
   return {
-    transports: [
-      new transports.Console({
-        format: format.combine(format.timestamp(), nestLikeConsoleFormat()),
-      }),
-    ],
+    format: format.combine(format.timestamp(), outputFormat),
+    transports: [new transports.Console()],
   };
 };
 
